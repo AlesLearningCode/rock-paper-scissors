f1d36fdf0cf41398eedbeef1b492666a6cbe6e86
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-10-06 20:32:10 +0200

    Make a game functional
    Changed a function playRound so that machine always picks a different from the three(Rock,Paper,Scissors) by adding the function at the beginning
    also added a function game, so it prompts the player 5 times and game has 5 rounds, next added a scoreboard which says who won,
    added computer and player score count, getComputerChoice changed to array for better readability

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,58 @@
+//Computer choice randomization between rock, paper,scissors
 function getComputerChoice(){
+    let random = ["Rock","Paper","Scissors"];
+    let randomize = random[Math.floor(Math.random()*random.length)]
+    return randomize
+}
+
+const computerSelection = getComputerChoice();
+let playerSelection;
+//Round played player chooses their hand and computer chooses it's randomly
+function playRound(computerSelection,playerSelection){
+    //prompting player to choose
+    playerSelection = prompt("Choose a hand you want to play Rock, Paper or Scissors");
+    // calling function getcomputer choice so it picks randomly every turn
+    computerSelection = getComputerChoice();
+    if(computerSelection === "Rock"){
+        switch(playerSelection.toUpperCase()){
+            case "ROCK":
+                return console.log("It's a draw");
+            break;
+            break;playRound(computerSelection);
+            case "SCISSORS":
+                return console.log("You lose! Rock beats Scissors!"),computerScore++;
+            break;playRound(computerSelection);
+            case "PAPER":
+                return console.log("You win! Paper beats Rock"), playerScore++;
+            break;playRound(computerSelection);
+        }
+    }else if(computerSelection === "Paper"){
+        switch(playerSelection.toUpperCase()){
+            case "ROCK":
+                return console.log("You lose! Paper beats Rock"),computerScore++;
+            break;
+            case "SCISSORS":
+                return console.log("You win! Scissors beat Paper"),playerScore++;
+            break;playRound(computerSelection);
+            case "PAPER":
+                return console.log("It's a draw");
+            break;
+            }
+    }else{
+        switch(playerSelection.toUpperCase()){
+            case "ROCK":
+                return console.log("You win! Rock beats Scissors"),playerScore++;
+            break;
+            case "SCISSORS":
+                return console.log("It's a draw!")
+            break ;playRound(computerSelection);
+            case "PAPER":
+                return console.log("You lose! Scissors beat Paper!"),computerScore++;
+            break ;playRound(computerSelection);
+        }
+    }    
+}
+/*function getComputerChoice(){
     let randomize = parseInt(Math.random() *3);
     if(randomize === 2){
         return "Rock";
@@ -7,46 +61,28 @@ function getComputerChoice(){
     }else{
         return "Scissors" ;
     }
-}
-function playRound(playerSelection,computerSelection){
-    if(computerSelection === "Rock"){
-        switch(playerSelection){
-            case "Rock":
-                return "It's a draw!";
-            break;
-            case "Scissors":
-                return "You lose! Rock beats Scissors!";
-            break;
-            case "Paper":
-                return "You win! Paper beats Rock!";
-            break;
-        }
-    }else if(computerSelection === "Paper"){
-        switch(playerSelection){
-            case "Rock":
-                return "You lose! Paper beats Rock!";
-            break;
-            case "Scissors":
-                return "You win! Scissors beat Paper!"
-            break;
-            case "Paper":
-                return "It's a draw"
-        }
-    }else{
-        switch(playerSelection){
-            case "Rock":
-                return "You win! Rock beats Scissors";
-            break;
-            case "Scissors":
-                return "It's a draw!"
-            break;
-            case "Paper":
-                return "You lose! Scissors beat Paper!"
-        }
+}*/
+// let playerSelection = p if (playerScore > computerScore) {
+    let computerScore , playerScore;
+    // a function to call playRound 5 times and decide who is winner
+    function game(){
+    computerScore = 0;
+    playerScore = 0;
+    playRound(computerSelection,playerSelection)
+    playRound(computerSelection,playerSelection)
+    playRound(computerSelection,playerSelection)
+    playRound(computerSelection,playerSelection)
+    playRound(computerSelection,playerSelection)
+    if (playerScore > computerScore) {
+        console.log('You win!     (You: ' + playerScore + '  Computer: ' + computerScore + 
+        ')');
+    } else {
+        console.log('You lose...  (You: ' + playerScore + '  Computer: ' + computerScore + 
+        ')');
     }
 }
 
-let playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game()
+
+
 
