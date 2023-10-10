dad507320ab2656106f5edf8b1e1d34658fbc1b8
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-10-10 19:46:12 +0200

    Make functional end score and assigning points
    Next have to do a reset button, or reset after getting 5 points

--- js/javascript.js
+++ js/javascript.js
@@ -1,86 +1,95 @@
+    const btnRock = document.querySelector(`.rock`);
+    const btnScissors = document.querySelector(`.scissors`);
+    const btnPaper = document.querySelector(`.paper`);
+    const roundMessage = document.querySelector(`.roundMsg`)
+    const endScore = document.querySelector(`.endScore`)
+    const buttons = document.querySelectorAll(`button`)
+
+    const computerSelection = getComputerChoice();
+    let playerSelection; 
+    let computerScore = 0;
+    let playerScore = 0;
+       
     //Computer choice randomization between rock, paper,scissors
     function getComputerChoice(){
     let computerInput = ["Rock","Paper","Scissors"];
     let randomizeArray = computerInput[Math.floor(Math.random()*computerInput.length)];
     return randomizeArray;
 }
-    
-    let playerSelection;
 
     function playRound(computerSelection, playerSelection){
     computerSelection = getComputerChoice();
+    
     if(computerSelection === "Rock"){
         switch(playerSelection){
             case "ROCK":
-                return console.log("It's a draw");
+            roundMessage.textContent = "It's a draw";
             break;
             case "SCISSORS":
-                return console.log("You lose! Rock beats Scissors!"),computerScore++;
+            roundMessage.textContent = "You lose! Rock beats Scissors!", 
+            computerScore++;
             break;
             case "PAPER":
-                return console.log("You win! Paper beats Rock"), playerScore++;
+            roundMessage.textContent ="You win! Paper beats Rock", 
+            playerScore++;
             break;
         }
     }else if(computerSelection === "Paper"){
         switch(playerSelection){
             case "ROCK":
-                return console.log("You lose! Paper beats Rock"),computerScore++;
+            roundMessage.textContent ="You lose! Paper beats Rock",
+            computerScore++;
             break;
             case "SCISSORS":
-                return console.log("You win! Scissors beat Paper"),playerScore++;
+            roundMessage.textContent ="You win! Scissors beat Paper",
+            playerScore++;
             break;
             case "PAPER":
-                return console.log("It's a draw");
+            roundMessage.textContent ="It's a draw";
             break;
             }
     }else{
         switch(playerSelection){
             case "ROCK":
-                return console.log("You win! Rock beats Scissors"),playerScore++;
+            roundMessage.textContent ="You win! Rock beats Scissors", 
+            playerScore++;
             break;
             case "SCISSORS":
-                return console.log("It's a draw!")
-            break ;
+            roundMessage.textContent ="It's a draw!"
+            break;
             case "PAPER":
-                return console.log("You lose! Scissors beat Paper!"),computerScore++;
-            break ;
+            roundMessage.textContent = "You lose! Scissors beat Paper!",  
+            computerScore++;
+            break;
         }
-    }    
-}
-    const computerSelection = getComputerChoice();
-    let computerScore , playerScore;
-   
-    function game(){
-    computerScore = 0;
-    playerScore = 0;
-    /*for(let i = 1; i <= 5; i++ ){
-        playRound(i)
-    }*/
-    if (playerScore > computerScore) {
-        console.log('You win!...   (Your score: ' + playerScore + '  Computer: ' + computerScore + 
-        ')');
-    } else if(computerScore > playerScore) {
-        console.log('You lose...  (Your score: ' + playerScore + '  Computer: ' + computerScore + 
-        ')');
-    }else{
-        console.log(`It's a draw... (Your score: ` + playerScore + `  Computer: ` + computerScore + 
-        `)`);
     }
 }
+       
+
+    function playGame(){
+        btnRock.addEventListener(`click`, () => {
+            playRound(computerSelection,"ROCK")
+            endScore.textContent = `${playerScore} - ${computerScore}`
+    });
+        btnScissors.addEventListener(`click`, () => {
+            playRound(computerSelection,"SCISSORS")
+            endScore.textContent = `${playerScore} - ${computerScore}`
+    });
+        btnPaper.addEventListener(`click`, () => {
+            playRound(computerSelection,"PAPER")
+            endScore.textContent = `${playerScore} - ${computerScore}`
+        })
+        buttons.forEach((button) => {
+            button.addEventListener(`click`, () => {
+                if (playerScore >= 5){
+                    endScore.textContent = 'You win!...   (Your score: ' + playerScore + '  Computer: ' + computerScore + 
+                ')';
+                } else if(computerScore >= 5){
+                endScore.textContent = 'You lose!...   (Your score: ' + playerScore + '  Computer: ' + computerScore + 
+                ')';
+                }
+            });
+        });
     
-
-    const btnRock = document.querySelector(`.rock`);
-    const btnScissors = document.querySelector(`.scissors`);
-    const btnPaper = document.querySelector(`.paper`);
-    btnRock.addEventListener(`click`, function Rock(){
-        playRound(computerSelection,"ROCK")
-     })
-  
-    btnScissors.addEventListener(`click`, () => {
-        playRound(computerSelection,"SCISSORS")
-     })
-   
-    btnPaper.addEventListener(`click`, () => {
-        playRound(computerSelection,"PAPER")
-    })
-
+} 
+playGame()
